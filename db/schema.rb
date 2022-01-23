effeee5bd913078fefdd797580bce99ca0ef2e93
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-01-23 16:25:44 +0530

    Added notes functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_182941) do
+ActiveRecord::Schema.define(version: 2022_01_23_074857) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 2022_01_14_182941) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "notes", "users"
 end
