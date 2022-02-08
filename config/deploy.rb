e1c3b69d7abc02873bea91c145e86f9657770c9d
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-02-08 19:47:21 +0530

    Setup capistrano in the project

---
+++ config/deploy.rb
@@ -0,0 +1,97 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.16.0"
+
+# Change these
+# server '143.244.136.247', port: 22, roles: [:web, :app, :db], primary: true
+set :repo_url,        'git@github.com:sreeram-venkitesh/devise_blog.git'
+set :application,     'devise_blog'
+set :user,            'sreeram'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+
+set :default_env, {
+  "PATH" => "/home/#{fetch(:user)}/.nvm/versions/node/v16.4.2/bin:$PATH"
+}
+
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+set :puma_phased_restart, true
+set :puma_enable_socket_service, true
+set :puma_service_unit_name, "puma"
+
+## Defaults:
+# set :scm,           :git
+set :branch,        :master
+# set :format,        :pretty
+set :log_level,     :debug
+set :keep_releases, 5
+
+## Linked Files & Directories (Default None):
+set :linked_files, %w{config/database.yml config/master.key}
+set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+append :rbenv_map_bins, "puma", "pumactl"
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before "deploy:compile_assets", "yarn:install"
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
+
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
