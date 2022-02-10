8d2d0c0fe128548f222bf02d2f49ccff050e1685
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-02-10 16:29:20 +0530

    Added ansible playbook and configs for development environment

---
+++ config/deploy/development.rb
@@ -0,0 +1,10 @@
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :stage, "development"
+
+server "143.244.130.180",
+  user: "deploy",
+  roles: %w{web app db},
+  ssh_options: {
+    forward_agent: true,
+    auth_methods: %w(publickey)
+  }
