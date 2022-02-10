c2038e2af3b74fc3ee50aed05db4bd81eaa45654
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-02-11 00:10:08 +0530

    Fixed user, node version, yarn install, listen gem in development

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -2,7 +2,7 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :stage, "development"
 
 server "143.244.130.180",
-  user: "deploy",
+  user: "sreeram",
   roles: %w{web app db},
   ssh_options: {
     forward_agent: true,
